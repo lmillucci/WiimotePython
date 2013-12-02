252b6e82441c57185d2634946170ba32b952c85f
Lorenzo <lorenzo.millucci@outlook.com>
2013-12-02 15:02:05 +0100

    riconoscimento della pressione del tasto DX e SX

--- wiimote.py
+++ wiimote.py
@@ -3,11 +3,25 @@ import time
 
 print("Premi 1+2 per connettere il wiimote")
 
-time.sleep(1)
+time.sleep(2)
 
 wm=cwiid.Wiimote()
 
 for i in range(16):
 	wm.led=i
-	time.sleep(1)
+	time.sleep(0.5)
+	
+wm.rpt_mode = cwiid.RPT_BTN
+
+while True:
+	button = wm.state['buttons']
+	
+	# & serve per fare il confronto bit a bit
+	if(button & cwiid.BTN_LEFT):
+		print("Ho premuto il tasto sinistro ",button)
+	
+	if(button & cwiid.BTN_RIGHT):
+		print("Ho premuto il tasto destro",button)
+	
+	time.sleep(0.1)
 
